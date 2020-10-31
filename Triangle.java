085fe16285d77333aa8cd359795e50656239b238
Jasmine <jwang2367@gmail.com>
2020-10-31 15:22:10 -0400

    Caught and slaughtered bugs

--- Triangle.java
+++ Triangle.java
@@ -19,14 +19,18 @@ public class Triangle{
 
   public double getArea() {
     double sp = (0.5 * getPerimeter());
-    return Math.sqrt(sp * (sp - v1.distanceTo(v2)) * (sp - v1.distanceTo(v2)) * (sp - v1.distanceTo(v2)));
+    return Math.sqrt(sp * (sp - v1.distanceTo(v2)) * (sp - v2.distanceTo(v3)) * (sp - v3.distanceTo(v1)));
   }
 
   public String classify() {
-    double p1 = (Math.round(v1.distanceTo(v2) * 10000))/10000;
-    double p2 = (Math.round(v2.distanceTo(v2) * 10000))/10000;
-    double p3 = (Math.round(v3.distanceTo(v2) * 10000))/10000;
-    if (p1 == p2 && p2 == p3 && p3 == p1) {
+    double p1 = (Math.round(v1.distanceTo(v2) * 10000));
+    double p2 = (Math.round(v2.distanceTo(v3) * 10000));
+    double p3 = (Math.round(v3.distanceTo(v1) * 10000));
+    p1 /= 10000;
+    p2 /= 10000;
+    p3 /= 10000;
+
+    if (p1 == p2 && p2 == p3) {
       return "equilateral";
     }
     else if (p1 == p2 || p1 == p3 || p2 == p3) {
