37f7885e68e77c46bc52fcd40de996a578212d2c
Jasmine <jwang2367@gmail.com>
2020-10-31 13:29:29 -0400

    added equals

--- Point.java
+++ Point.java
@@ -22,4 +22,8 @@ public class Point{
     return Math.sqrt(Math.pow((this.x - other.x), 2) + Math.pow((this.y - other.y), 2));
   }
 
+  public boolean equals(Point other){
+    return (this.x == other.x && this.y == other.y);
+  }
+
 }
--- Tester.java
+++ Tester.java
@@ -2,8 +2,12 @@ public class Tester {
   public static void main(String[] args) {
     Point myPoint = new Point(3.0, 4.0);
     Point newPoint = new Point(0, 0);
+    Point thisPoint = new Point(3.0, 4.0);
     System.out.println(myPoint.getX());
     System.out.println(myPoint.getY());
     System.out.println(myPoint.distanceTo(newPoint)); //expecting 5.0
+    System.out.println(myPoint.equals(newPoint)); //expecting false
+    System.out.println(myPoint.equals(thisPoint)); //expecting true
+
   }
 }
